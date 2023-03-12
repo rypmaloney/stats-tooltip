bdd36425f842d34a7c4d2dd272b3dbaf515ceb60
Ryan Maloney <rypmaloney@gmail.com>
2023-03-12 16:36:29 -0400

    Make tooltip table. Find names Regex. Loop through all elements find names, and fetch data. Basic display info on tooltip.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # stats-tooltip
 Extension to create hover over tooltips for MLB active players in articles.
 
-Player ID mapping provided by: https://www.smartfantasybaseball.com/
+Player ID mapping provided by: https://www.smartfantasybaseball.com
--- content.js
+++ content.js
@@ -9,22 +9,42 @@ async function loadJsonFile(filename) {
     }
 }
 
-const jsonDasta = await loadJsonFile('stript.json');
+function getWordPairs(string) {
+    let words = string.split(' ');
+    let pairs = [];
+    for (let i = 0; i < words.length - 1; i++) {
+        pairs.push(words[i] + ' ' + words[i + 1]);
+    }
+    return pairs;
+}
 
 function findPlayerNames(jsonData, paragraph) {
-    const text = paragraph;
+    const nameRegex =
+        /(?:\b(?!([A-Z][a-z]+)\.))(?:\b[A-Z][a-z]+\b\s){1,2}\b[A-Z][a-z]+(?:[-'][A-Za-z][a-z]+)?\b/g;
 
-    const nameRegex = /([A-Z][a-z]+)\s+([A-Z][a-z]+)/g;
-    let match;
-
-    while ((match = nameRegex.exec(text)) !== null) {
-        const fullName = match[0];
-
-        if (jsonData.hasOwnProperty(fullName)) {
-            const graphId = jsonData[fullName].graph_id;
-            return { id: graphId, name: fullName };
-        }
+    const matches = paragraph.match(nameRegex);
+    console.log(matches);
+    if (matches === null) {
+        return false;
     }
+    const names = [];
+    matches.forEach((match) => {
+        if (jsonData.hasOwnProperty(match)) {
+            const graphId = jsonData[match].graph_id;
+            names.push({ id: graphId, name: match });
+        } else if (match.split(' ').length > 2) {
+            const split_names = getWordPairs(match);
+
+            split_names.forEach((match) => {
+                if (jsonData.hasOwnProperty(match)) {
+                    const graphId = jsonData[match].graph_id;
+                    names.push({ id: graphId, name: match });
+                }
+            });
+        }
+    });
+
+    return names;
 }
 
 async function fetchStats(playerId) {
@@ -40,16 +60,78 @@ async function fetchStats(playerId) {
         console.error(error);
     }
 }
-async function example() {
-    try {
-        const data = await fetchStats(10155);
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
-}
 
-searchForNames(
-    jsonData,
-    "<p>There are many variations Tony Abreu of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>"
-);
+const createTooltipTable = (data, keysToInclude) => {
+    const table = document.createElement('table');
+    const headerRow = table.insertRow();
+    for (let i = 0; i < keysToInclude.length; i++) {
+        const headerCell = headerRow.insertCell();
+        headerCell.textContent = keysToInclude[i];
+    }
+
+    // Get the current year and the two previous years
+    const currentYear = new Date().getFullYear();
+    const previousYears = [currentYear - 1, currentYear - 2];
+
+    // Create a set of years that have already been added to the table
+    const addedYears = new Set();
+
+    // Create a row for each object in the data that matches the current or previous years
+    for (var j = 0; j < data.length; j++) {
+        const aseasonYear = parseInt(data[j]['aseason']);
+        if (
+            previousYears.includes(aseasonYear) &&
+            !addedYears.has(aseasonYear)
+        ) {
+            const dataRow = table.insertRow();
+            for (let k = 0; k < keysToInclude.length; k++) {
+                let tdata = data[j][keysToInclude[k]];
+                if (keysToInclude[k] != 'Season') {
+                    tdata = Math.round(tdata * 10) / 10;
+                }
+                const dataCell = dataRow.insertCell();
+                dataCell.innerHTML = tdata;
+            }
+            addedYears.add(aseasonYear);
+        }
+    }
+
+    // Return the HTML code for the table
+    return table.outerHTML;
+};
+
+(async () => {
+    const elements = document.querySelectorAll('a, p, td, h1, h2, h3, h4');
+    const jsonData = await loadJsonFile('stript.json');
+
+    for (i = 0; i < elements.length; i++) {
+        const foundPlayers = findPlayerNames(jsonData, elements[i].innerText);
+        const originalText = elements[i].innerHTML;
+        let replacedText = originalText;
+
+        if (foundPlayers) {
+            for (let j = 0; j < foundPlayers.length; j++) {
+                try {
+                    const data = await fetchStats(foundPlayers[j]['id']);
+
+                    const table = createTooltipTable(data['data'], [
+                        'Season',
+                        'Age',
+                        'G',
+                        'wOBA',
+                        'wRC+',
+                        'WAR',
+                    ]);
+
+                    replacedText = replacedText.replace(
+                        foundPlayers[j]['name'],
+                        `<span class="tooltip">${foundPlayers[j]['name']}<span class="tooltiptext">${table}</span></span>`
+                    );
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+        elements[i].innerHTML = replacedText;
+    }
+})();
